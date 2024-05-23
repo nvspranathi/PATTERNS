612355a66ecee447fcd47b420ecc4e3e09e5b8f8
nvspranathi <19h71a0453@gmail.com>
2024-05-23 11:50:01 +0530

    first pattern solid rectangle

---
+++ PATTERNS/SolidRect.java
@@ -0,0 +1,13 @@
+public class SolidRect{
+	public static void main(String[] args){
+		int rows = 4;
+		int columns = 5;
+		// Quesn: print a rectangle with i rows, j columns using a *
+		for(int i=0;i<rows;i++){
+			for(int j=0; j<columns; j++){
+				System.out.print("*");
+				}
+			System.out.println("");
+			}
+		}
+}
