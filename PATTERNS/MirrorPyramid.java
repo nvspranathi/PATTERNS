bb38b163f46a8e5a55d76f57c1ff36b72424709f
nvspranathi <19h71a0453@gmail.com>
2024-05-29 11:49:54 +0530

    Mirror Pyramid code

--- PATTERNS/MirrorPyramid.java
+++ PATTERNS/MirrorPyramid.java
@@ -11,6 +11,15 @@ public class MirrorPyramid{
 					System.out.print("*");
 				}
 			}
+/*
+or instead of inner for loop :
+for(int j=1;j<=n-1;j++){
+//print(" ")
+}
+for(int j=1;j,=i;j++){
+//print("*")
+}
+*/
 			System.out.println();
 		}
 	}
