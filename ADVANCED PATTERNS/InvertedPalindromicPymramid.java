aa98d16cfbceef477d5cea1ff7d708fb261203b7
nvspranathi <19h71a0453@gmail.com>
2024-07-19 09:27:36 +0530

    Inverted Palindromic Pyramid (extra)

---
+++ ADVANCED PATTERNS/InvertedPalindromicPymramid.java
@@ -0,0 +1,18 @@
+public class InvertedPalindromicPymramid{
+	public static void main(String[] args){
+		int n = 5;
+		for(int i=n;i>=1;i--){
+			for(int j=1;j<=(n-i);j++){
+				System.out.print("  ");
+			}
+			for(int k=i;k>=1;k--){
+				System.out.print(k+" ");
+			}
+			for(int p=2;p<=i;p++){
+				System.out.print(p+" ");
+			}
+		System.out.println();
+		}
+
+	}
+}
